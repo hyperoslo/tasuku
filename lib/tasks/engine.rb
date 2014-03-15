6d2f2ae943ecb84283018807bb75249c70ba6741
Johannes Gorset <jgorset@gmail.com>
2014-03-15 18:43:22 +0100

    Share factories with client applications

--- lib/tasks/engine.rb
+++ lib/tasks/engine.rb
@@ -2,6 +2,10 @@ module Tasks
   class Engine < ::Rails::Engine
     isolate_namespace Tasks
 
+    initializer 'tasks.factories', after: 'factory_girl.set_factory_paths' do
+      FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
