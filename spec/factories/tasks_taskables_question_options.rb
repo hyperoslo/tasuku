db88211eed9af4929a634df9296e5e8b3e16ceba
Johannes Gorset <jgorset@gmail.com>
2014-03-16 22:40:17 +0100

    Implement question views

--- spec/factories/tasks_taskables_question_options.rb
+++ spec/factories/tasks_taskables_question_options.rb
@@ -1,8 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :tasks_taskables_question_option, aliases: [:question_option], class: 'Tasks::Taskables::Question::Option' do
-    text "MyText"
+    text Faker::Lorem.word
     question nil
   end
 end
--- spec/factories/tasks_taskables_questions.rb
+++ spec/factories/tasks_taskables_questions.rb
@@ -3,5 +3,9 @@
 FactoryGirl.define do
   factory :tasks_taskables_question, aliases: [:question], class: 'Tasks::Taskables::Question' do
     text "MyText"
+
+    factory :tasks_taskables_question_with_options, aliases: [:question_with_options] do
+      after(:create) { |question| create_list :question_option, 3, question: question }
+    end
   end
 end
