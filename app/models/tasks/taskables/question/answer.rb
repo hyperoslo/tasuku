2a9254bf219e69039a1246732c28533eb199082b
Johannes Gorset <jgorset@gmail.com>
2014-03-16 23:31:15 +0100

    Prohibit authors from answering the same question more than once

--- app/models/tasks/taskables/question/answer.rb
+++ app/models/tasks/taskables/question/answer.rb
@@ -3,7 +3,17 @@ module Tasks
     belongs_to :option
     belongs_to :author, polymorphic: true
 
+    has_one :question, through: :option
+
     validates :option_id, presence: true
     validates :author_id, :author_type, presence: true
+
+    validate :can_only_answer_each_question_once
+
+    private
+
+    def can_only_answer_each_question_once
+      errors.add :base, I18n.t('tasks.taskables.questions.answers.already_answered') if question.answers.find_by author: author
+    end
   end
 end
--- spec/models/tasks/taskables/question/answer_spec.rb
+++ spec/models/tasks/taskables/question/answer_spec.rb
@@ -2,5 +2,24 @@ require 'spec_helper'
 
 module Tasks
   describe Taskables::Question::Answer do
+    describe 'validations' do
+      describe '#can_only_answer_each_question_once' do
+        let(:user)      { create :user }
+        let(:question)  { create :question_with_options }
+        let(:duplicate) { build :question_answer, option: question.options.first, question: question, author: user }
+
+        before do
+          create :question_answer, option: question.options.first, question: question, author: user
+        end
+
+        it 'prohibits an author from answering the same question more than once' do
+          expect(duplicate).not_to be_valid
+        end
+
+        it 'should have errors' do
+          expect(duplicate.errors_on(:base)).to include I18n.t('tasks.taskables.questions.answers.already_answered')
+        end
+      end
+    end
   end
 end
