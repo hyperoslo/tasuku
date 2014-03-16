c7b6457b7934f9634a3080cf872ba9868e457dfb
Johannes Gorset <jgorset@gmail.com>
2014-03-16 23:52:28 +0100

    Display the answer if a user already answered a question

--- app/helpers/tasks/application_helper.rb
+++ app/helpers/tasks/application_helper.rb
@@ -1,4 +1,7 @@
 module Tasks
   module ApplicationHelper
+    def current_author
+      send Tasks.config.author
+    end
   end
 end
--- spec/views/tasks/taskables/questions/answers/_form.html.erb_spec.rb
+++ spec/views/tasks/taskables/questions/answers/_form.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe 'tasks/taskables/questions/_question.html.erb' do
+describe 'tasks/taskables/questions/answers/_form.html.erb' do
   let(:user)     { create :user }
   let(:question) { create :question_with_options }
 
   before do
-    render partial: 'tasks/taskables/questions/question.html.erb', locals: {
+    render partial: 'tasks/taskables/questions/answers/form.html.erb', locals: {
       question: question
     }
   end
