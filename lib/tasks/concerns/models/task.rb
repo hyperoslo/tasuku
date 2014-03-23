cac9b7361c7e29543b4322a84495f11f6456c23a
Johannes Gorset <jgorset@gmail.com>
2014-03-23 22:23:19 +0100

    Destroy the taskable upon destroying a task

--- lib/tasks/concerns/models/task.rb
+++ lib/tasks/concerns/models/task.rb
@@ -2,7 +2,7 @@ module Tasks::Concerns::Models::Task
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :taskable, polymorphic: true
+    belongs_to :taskable, polymorphic: true, dependent: :destroy
 
     # TODO: These should be SQL. But that's hard.
     scope :completed_by,  ->(author) { all.select { |task| task.completed_by? author }}
