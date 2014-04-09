9a57a66850cc5ae4b354ea286bcc15cdf8093d87
Sindre Moen <sindrenm@gmail.com>
2014-04-09 13:13:54 +0200

    Move TasksController into a concern

--- app/controllers/tasks/tasks_controller.rb
+++ app/controllers/tasks/tasks_controller.rb
@@ -2,15 +2,6 @@ require_dependency "tasks/application_controller"
 
 module Tasks
   class TasksController < ApplicationController
-    before_action :set_task
-
-    def show
-    end
-
-    private
-
-    def set_task
-      @task = Task.find params[:id]
-    end
+    include Concerns::Controllers::TasksController
   end
 end
