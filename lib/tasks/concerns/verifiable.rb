2181c0a75dea36680847226e23767ef4252587be
Sindre Moen <sindrenm@gmail.com>
2014-04-14 13:47:19 +0200

    Destroy verifications when destroying verifiables

--- lib/tasks/concerns/verifiable.rb
+++ lib/tasks/concerns/verifiable.rb
@@ -2,7 +2,7 @@ module Tasks::Concerns::Verifiable
   extend ActiveSupport::Concern
 
   included do
-    has_one :verification, as: :verifiable, class_name: '::Tasks::Taskables::Verification'
+    has_one :verification, as: :verifiable, class_name: '::Tasks::Taskables::Verification', dependent: :destroy
     has_one :task, through: :verification, class_name: '::Tasks::Task'
     has_many :confirmations, through: :verification, class_name: '::Tasks::Taskables::Verification::Confirmation'
   end
