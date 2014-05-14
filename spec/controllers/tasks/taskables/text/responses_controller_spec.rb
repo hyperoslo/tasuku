d58d8b958cb2a597bfb2c9288fdc15d21713424b
Pål Andre Sundt <pal@rubynor.com>
2014-05-14 14:17:17 +0200

    Add specs

---
+++ spec/controllers/tasks/taskables/text/responses_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+module Tasks
+  describe Taskables::Text::ResponsesController do
+    routes { Tasks::Engine.routes }
+
+    describe "POST 'create'" do
+      it 'routes' do
+        expect(post: '/text/requests/1/responses').to route_to(
+          action: 'create',
+          controller: 'tasks/taskables/text/responses',
+          request_id: '1'
+        )
+      end
+
+      context 'with a response' do
+        let(:user)          { create :user }
+        let(:text_request) { create :text_request }
+        let(:params)        { { request_id: text_request.id, taskables_text_response: { text: 'Yes hello this is dog?' } } }
+
+        before { request.env['HTTP_REFERER'] = 'http://example.org' }
+        before { expect(subject).to receive(:current_user).and_return(user) }
+
+        it_behaves_like 'redirectable' do
+          let(:action) { :create }
+          let(:verb)   { :post }
+        end
+
+        it 'creates a new response' do
+          post :create, params
+
+          expect(text_request.responses.count).to eq 1
+        end
+      end
+    end
+  end
+end
