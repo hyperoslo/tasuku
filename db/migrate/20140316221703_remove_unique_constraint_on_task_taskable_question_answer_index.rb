5e5372c080ebf6a48b4246eac6bb4c3234a1bf16
Pål Andre Sundt <pal@rubynor.com>
2014-05-15 14:25:44 +0200

    Remove 'change' and use 'up' to avoid errors on rollbacks

--- db/migrate/20140316221703_remove_unique_constraint_on_task_taskable_question_answer_index.rb
+++ db/migrate/20140316221703_remove_unique_constraint_on_task_taskable_question_answer_index.rb
@@ -1,5 +1,5 @@
 class RemoveUniqueConstraintOnTaskTaskableQuestionAnswerIndex < ActiveRecord::Migration
-  def change
+  def up
     remove_index :tasks_taskables_question_answers, name: 'tasks_taskable_question_answers_author'
     add_index :tasks_taskables_question_answers, [:author_id, :author_type], name: 'tasks_taskable_question_answers_author'
   end
--- db/migrate/20140320123047_remove_unique_constraint_on_confirmation_author.rb
+++ db/migrate/20140320123047_remove_unique_constraint_on_confirmation_author.rb
@@ -1,5 +1,5 @@
 class RemoveUniqueConstraintOnConfirmationAuthor < ActiveRecord::Migration
-  def change
+  def up
     remove_index :tasks_taskables_verification_confirmations, name: 'tasks_taskable_verification_confirmation_author'
     add_index :tasks_taskables_verification_confirmations, ['author_id', 'author_type'], name: 'tasks_taskable_verification_confirmation_author'
   end
