e88acd4d4425ab1e15a452a2a9705d9d6b8fa36c
Felipe Espinoza <felipe@hyper.no>
2014-05-27 16:08:32 +0200

    fix chnage tasks to tasuku migration

--- db/migrate/20140526130322_change_tasks_tables_to_tasuku_tables.rb
+++ db/migrate/20140526130322_change_tasks_tables_to_tasuku_tables.rb
@@ -1,5 +1,19 @@
 class ChangeTasksTablesToTasukuTables < ActiveRecord::Migration
   def change
+    remove_index 'tasks_taskables_image_responses', name: 'tasks_taskable_image_responses_author'
+    remove_index 'tasks_taskables_image_responses', name: 'index_tasks_taskables_image_responses_on_request_id'
+    remove_index 'tasks_taskables_question_answers', name: 'tasks_taskable_question_answers_author'
+    remove_index 'tasks_taskables_question_options', name: 'index_tasks_taskables_question_options_on_question_id'
+    remove_index 'tasks_taskables_question_votes', name: 'index_tasks_taskables_question_votes_on_answer_id'
+    remove_index 'tasks_taskables_question_votes', name: 'index_tasks_taskables_question_votes_on_option_id'
+    remove_index 'tasks_taskables_text_responses', name: 'tasks_taskable_text_responses_author'
+    remove_index 'tasks_taskables_url_responses', name: 'tasks_taskable_url_responses_author'
+    remove_index 'tasks_taskables_url_responses', name: 'index_tasks_taskables_url_responses_on_request_id'
+    remove_index 'tasks_taskables_verification_confirmations', name: 'tasks_taskable_verification_confirmation_author'
+    remove_index 'tasks_taskables_verification_confirmations', name: 'tasks_taskable_verification_confirmation_verification'
+    remove_index 'tasks_taskables_verifications', name: 'index_tasks_taskable_verifications_verifiable'
+    remove_index 'tasks_tasks', name: 'index_tasks_tasks_on_taskable_id_and_taskable_type'
+
     rename_table 'tasks_taskables_image_requests', 'tasuku_taskables_image_requests'
     rename_table 'tasks_taskables_image_responses', 'tasuku_taskables_image_responses'
     rename_table 'tasks_taskables_question_answers', 'tasuku_taskables_question_answers'
@@ -14,18 +28,18 @@ class ChangeTasksTablesToTasukuTables < ActiveRecord::Migration
     rename_table 'tasks_taskables_verifications', 'tasuku_taskables_verifications'
     rename_table 'tasks_tasks', 'tasuku_tasks'
 
-    rename_index 'tasuku_taskables_image_responses', 'tasks_taskable_image_responses_author', 'tasuku_taskable_image_responses_author'
-    rename_index 'tasuku_taskables_image_responses', 'index_tasks_taskables_image_responses_on_request_id', 'index_tasuku_taskables_image_responses_on_request_id'
-    rename_index 'tasuku_taskables_question_answers', 'tasks_taskable_question_answers_author', 'tasuku_taskable_question_answers_author'
-    rename_index 'tasuku_taskables_question_options', 'index_tasks_taskables_question_options_on_question_id', 'index_tasuku_taskables_question_options_on_question_id'
-    rename_index 'tasuku_taskables_question_votes', 'index_tasks_taskables_question_votes_on_answer_id', 'index_tasuku_taskables_question_votes_on_answer_id'
-    rename_index 'tasuku_taskables_question_votes', 'index_tasks_taskables_question_votes_on_option_id', 'index_tasuku_taskables_question_votes_on_option_id'
-    rename_index 'tasuku_taskables_text_responses', 'tasks_taskable_text_responses_author', 'tasuku_taskable_text_responses_author'
-    rename_index 'tasuku_taskables_url_responses', 'tasks_taskable_url_responses_author', 'tasuku_taskable_url_responses_author'
-    rename_index 'tasuku_taskables_url_responses', 'index_tasks_taskables_url_responses_on_request_id', 'index_tasuku_taskables_url_responses_on_request_id'
-    rename_index 'tasuku_taskables_verification_confirmations', 'tasks_taskable_verification_confirmation_author', 'tasuku_taskable_verification_confirmation_author'
-    rename_index 'tasuku_taskables_verification_confirmations', 'tasks_taskable_verification_confirmation_verification', 'tasuku_taskable_verification_confirmation_verification'
-    rename_index 'tasuku_taskables_verifications', 'index_tasks_taskable_verifications_verifiable', 'index_tasuku_taskable_verifications_verifiable'
-    rename_index 'tasuku_tasks', 'index_tasuku_tasks_on_taskable_id_and_taskable_type', 'index_tasks_tasks_on_taskable_id_and_taskable_type'
+    add_index 'tasuku_taskables_image_responses', ['author_id', 'author_type'], name: 'tasuku_taskable_image_responses_author'
+    add_index 'tasuku_taskables_image_responses', ['request_id'], name: 'index_tasuku_taskables_image_responses_on_request_id'
+    add_index 'tasuku_taskables_question_answers', ['author_id', 'author_type'], name: 'tasuku_taskable_question_answers_author'
+    add_index 'tasuku_taskables_question_options', ['question_id'], name: 'index_tasuku_taskables_question_options_on_question_id'
+    add_index 'tasuku_taskables_question_votes', ['answer_id'], name: 'index_tasuku_taskables_question_votes_on_answer_id'
+    add_index 'tasuku_taskables_question_votes', ['option_id'], name: 'index_tasuku_taskables_question_votes_on_option_id'
+    add_index 'tasuku_taskables_text_responses', ['author_id', 'author_type'], name: 'tasuku_taskable_text_responses_author', unique: true
+    add_index 'tasuku_taskables_url_responses', ['author_id', 'author_type'], name: 'tasuku_taskable_url_responses_author', unique: true
+    add_index 'tasuku_taskables_url_responses', ['request_id'], name: 'index_tasuku_taskables_url_responses_on_request_id'
+    add_index 'tasuku_taskables_verification_confirmations', ['author_id', 'author_type'], name: 'tasuku_taskable_verification_confirmation_author'
+    add_index 'tasuku_taskables_verification_confirmations', ['verification_id'], name: 'tasuku_taskable_verification_confirmation_verification'
+    add_index 'tasuku_taskables_verifications', ['verifiable_id', 'verifiable_type'], name: 'index_tasuku_taskable_verifications_verifiable', unique: true
+    add_index 'tasuku_tasks', ['taskable_id', 'taskable_type'], name: 'index_tasuku_tasks_on_taskable_id_and_taskable_type'
   end
 end
--- spec/factories/tasuku_taskables_question_votes.rb
+++ spec/factories/tasuku_taskables_question_votes.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :tasuku_taskables_question_vote, aliases: [:question_vote], :class => 'Taskables::Question::Vote' do
+  factory :tasuku_taskables_question_vote, aliases: [:question_vote], :class => 'Tasuku::Taskables::Question::Vote' do
     answer nil
     option nil
   end
