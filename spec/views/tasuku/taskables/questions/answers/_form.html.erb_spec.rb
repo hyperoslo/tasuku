8c9be02c971704ff0a26632390c494c697d6a37b
Felipe Espinoza <felipe@hyper.no>
2014-05-27 11:19:24 +0200

    fix views content

--- spec/views/tasuku/taskables/questions/answers/_form.html.erb_spec.rb
+++ spec/views/tasuku/taskables/questions/answers/_form.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe 'tasks/taskables/questions/answers/_form.html.erb' do
+describe 'tasuku/taskables/questions/answers/_form.html.erb' do
   let(:user)     { create :user }
   let(:question) { create :question_with_options }
 
   before do
-    render partial: 'tasks/taskables/questions/answers/form.html.erb', locals: {
+    render partial: 'tasuku/taskables/questions/answers/form.html.erb', locals: {
       question: question
     }
   end
