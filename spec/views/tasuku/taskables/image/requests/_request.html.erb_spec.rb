785e82fd4550f0393e49e1871910d351e23ec019
Pål Andre Sundt <pal@rubynor.com>
2014-06-11 17:20:10 +0200

    Add specs for the illustration partial

--- spec/views/tasuku/taskables/image/requests/_request.html.erb_spec.rb
+++ spec/views/tasuku/taskables/image/requests/_request.html.erb_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+require 'views/tasuku/taskables/illustration_spec'
 
 describe 'tasuku/taskables/image/requests/_request.html.erb' do
   let(:user) { create :user }
 
   context 'for image requests that have an image' do
-    let(:image_request) { create :image_request, image: fixture("doge.jpg") }
+    let(:image_request) { create :image_request, image: fixture("doge.jpg"), video_url: 'https://www.youtube.com/watch?v=z9Uz1icjwrM' }
 
     before do
       render partial: 'tasuku/taskables/image/requests/request.html.erb', locals: {
@@ -13,9 +14,8 @@ describe 'tasuku/taskables/image/requests/_request.html.erb' do
       }
     end
 
-    it 'renders the image' do
-      expect(rendered).to have_xpath "//img[contains(@src, \"#{image_request.image.url}\")]"
-    end
+    alias_method :request, :image_request
+    it_behaves_like 'illustrations'
   end
 
   context 'for image requests that have no image' do
@@ -28,8 +28,7 @@ describe 'tasuku/taskables/image/requests/_request.html.erb' do
       }
     end
 
-    it 'does not renders an image' do
-      expect(rendered).not_to have_xpath "//img"
-    end
+    alias_method :request, :image_request
+    it_behaves_like 'no illustrations'
   end
 end
