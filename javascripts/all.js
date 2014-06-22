2d4912db1929c6e0fd85a656b1b067bb3771513d
Johannes Gorset <jgorset@gmail.com>
2014-06-22 22:28:54 +0200

    Site updated to 07a96c7

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,4 +1,11 @@
 (function() {
-
+  $(function() {
+    $(window).on('resize', function() {
+      return $('.banner').css({
+        height: $(window).height()
+      });
+    });
+    return $(window).trigger('resize');
+  });
 
 }).call(this);
