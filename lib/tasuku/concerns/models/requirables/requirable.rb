8d68e02b972cc3663cf3e857f0b6b211a52b6968
Johannes Gorset <jgorset@gmail.com>
2014-09-14 11:38:06 +0200

    s/invers/inverse

--- lib/tasuku/concerns/models/requirables/requirable.rb
+++ lib/tasuku/concerns/models/requirables/requirable.rb
@@ -5,8 +5,8 @@ module Tasuku
     included do
       has_many :task_requirements
       has_many :requirements, through: :task_requirements
-      has_many :invers_tasks, class_name: "TaskRequirement", foreign_key: :requirement_id
-      has_many :required_for, through: :invers_tasks, source: :task
+      has_many :inverse_tasks, class_name: "TaskRequirement", foreign_key: :requirement_id
+      has_many :required_for, through: :inverse_tasks, source: :task
 
       accepts_nested_attributes_for :requirements, allow_destroy: true
 
