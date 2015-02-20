3835f29316bd10f8bfab269b117c4ac1b8e84906
Sam Figueroa <unimatrixzxero@gmail.com>
2015-02-20 07:35:35 +0100

    Refactor requirable
    - favor arel syntax instead of strings for where clauses
    - extract methods for better readability
    - change return statement on locked? method to avoid having to use
      a ternary operator

--- lib/tasuku/concerns/models/requirables/requirable.rb
+++ lib/tasuku/concerns/models/requirables/requirable.rb
@@ -11,24 +11,27 @@ module Tasuku
       accepts_nested_attributes_for :requirements, allow_destroy: true
 
       def locked? author
-        required_task_ids = self.task_requirements.pluck :requirement_id
         return false if required_task_ids.empty?
 
-        completed_task_ids = Tasuku::Task.completed_by(author).
-          where('tasuku_taskables_taskable_responses.task_id IN (?)', required_task_ids)
-
-        completed_task_ids.length.eql?(required_task_ids.length) ? false : true
+        completed_task_ids_for(author).length != required_task_ids.length
       end
 
       def locked_by_tasks author
         completed_task_ids = Tasuku::Task.completed_by(author).pluck(:id) || []
 
-        scope = self.requirements
-        scope = scope.where(
-          'tasuku_task_requirements.requirement_id NOT IN (?)', completed_task_ids
-        ) unless completed_task_ids.empty?
+        scope = requirements
+        scope = scope.where.not(tasuku_task_requirements: {requirement_id: completed_task_ids}) unless completed_task_ids.empty?
 
         scope
+      end
+
+      def completed_task_ids_for author
+        Tasuku::Task.completed_by(author).
+          where(tasuku_taskables_taskable_responses: {task_id:required_task_ids})
+      end
+
+      def required_task_ids
+        task_requirements.pluck :requirement_id
       end
     end
   end
