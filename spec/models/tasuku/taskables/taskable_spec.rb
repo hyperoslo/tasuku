daba52d5e14cf4e07c01d31be8d77ad1db9fb8df
Ole Henrik Skogstrøm <ole.henrik@hyper.no>
2015-04-21 15:36:16 +0200

    Update taskable spec

--- spec/models/tasuku/taskables/taskable_spec.rb
+++ spec/models/tasuku/taskables/taskable_spec.rb
@@ -30,6 +30,7 @@ module Tasuku
     describe '#completed_by?' do
       let(:taskable) { create :verification }
       let(:user)     { create :user }
+      let(:taskable_question) { create :question_with_options, options_count: 3, correct_option: 3 }
 
       context 'with an incomplete task' do
         it 'determines that the author has not completed it' do
@@ -46,7 +47,6 @@ module Tasuku
       end
 
       context 'with a incorrectly answered question task' do
-        let(:taskable_question) { create :question_with_options, options_count: 3, correct_option: 3 }
         before { create :question_answer, author: user, options: [taskable_question.options.first] }
 
         it 'determines that the author has not completed it' do
@@ -55,7 +55,6 @@ module Tasuku
       end
 
       context 'with a correctly answered question task' do
-        let(:taskable_question) { create :question_with_options, options_count: 3, correct_option: 3 }
         before { create :question_answer, author: user, options: [taskable_question.options.last] }
 
         it 'determines that the author has completed it' do
